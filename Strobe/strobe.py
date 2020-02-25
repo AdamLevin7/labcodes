7fb10c66fd1b346c0a5dd95df62be7ffafc07f6f
cwiens32 <cwiens32@gmail.com>
2020-02-25 10:46:12 -0800

    Prepocess image before subtraction, remove extra pixel changes

--- Strobe/strobe.py
+++ Strobe/strobe.py
@@ -25,6 +25,7 @@ Created on Thu Feb  6 12:35:18 2020
 import cv2
 import numpy as np
 from scipy import ndimage
+from skimage.measure import compare_ssim
 
 
 def strobe(filename, filesave, frames, searcharea=None, samp=240, thresh=60, bgint=5):
@@ -50,30 +51,30 @@ def strobe(filename, filesave, frames, searcharea=None, samp=240, thresh=60, bgi
             bgint -= 1
         #%% if this is the first frame
         if cnt == frames.iloc[0]:
+            # set counter for strobe frame number
+            strobeframe = 0
             # set frame and create copy
             cap.set(1,cnt)
             ret, img = cap.read()
             img_c1 = img.copy()
-            # if only searching within specified area
-            if searcharea is not None:
-                # identify current search area
-                crop = searcharea[cnt]
-                # set base black image, then store image
-                img = np.zeros(img.shape,'uint8')
-                img[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_c1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+            img_c1_blur = cv2.medianBlur(img_c1, 7)
             # set background (subtraction frame) and copy image
             cap.set(1,cnt+bgint)
             ret, img_bg = cap.read()
             img_bg_c1 = img_bg.copy()
+            img_bg_blur = cv2.medianBlur(img_bg_c1, 7)
             # if only searching within specified area
             if searcharea is not None:
                 # identify current search area
                 crop = searcharea[cnt]
                 # set base black image, then store image
+                img1 = np.zeros(img.shape,'uint8')
+                img1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_c1_blur[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+                # set base black image, then store image
                 img_bg = np.zeros(img.shape,'uint8')
-                img_bg[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_bg_c1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+                img_bg[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_bg_blur[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
             # find absolute difference between frames and then sum along BGR axis
-            diff = np.sum(cv2.absdiff(img_bg, img),axis=2)
+            diff = np.sum(cv2.absdiff(img_bg, img1),axis=2)
             # dilate image to fill holes
             keeppixels = ndimage.binary_dilation(diff>thresh, iterations=3)
             # set base black image, then store image
@@ -81,34 +82,33 @@ def strobe(filename, filesave, frames, searcharea=None, samp=240, thresh=60, bgi
             img_new[keeppixels,:] = img_c1[keeppixels,:]
             # set strobe image
             img_strobe = img_c1
-            # set current strobe image on
             
         #%% if a strobe frame that wasn't first
         elif (cnt == frames).any():
+            # iterate counter of strobe frame number
+            strobeframe += 1
             # set frame and create copy
             cap.set(1,cnt)
             ret, img = cap.read()
             img_c1 = img.copy()
-            # if only searching within specified area
-            if searcharea is not None:
-                # identify current search area
-                crop = searcharea[cnt]
-                # set base black image, then store image
-                img = np.zeros(img.shape,'uint8')
-                img[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_c1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+            img_c1_blur = cv2.medianBlur(img_c1, 7)
             # set background (subtraction frame)
             cap.set(1,cnt+bgint)
             ret, img_bg = cap.read()
             img_bg_c1 = img_bg.copy()
+            img_bg_blur = cv2.medianBlur(img_bg_c1, 7)
             # if only searching within specified area
             if searcharea is not None:
                 # identify current search area
                 crop = searcharea[cnt]
                 # set base black image, then store image
+                img1 = np.zeros(img.shape,'uint8')
+                img1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_c1_blur[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+                # set base black image, then store image
                 img_bg = np.zeros(img.shape,'uint8')
-                img_bg[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_bg_c1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+                img_bg[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_bg_blur[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
             # find absolute difference between frames and then sum along BGR axis
-            diff = np.sum(cv2.absdiff(img_bg, img),axis=2)
+            diff = np.sum(cv2.absdiff(img_bg, img1),axis=2)
             # dilate image to fill holes
             keeppixels = ndimage.binary_dilation(diff>thresh, iterations=3)
             # set base black image, then store image
@@ -117,7 +117,7 @@ def strobe(filename, filesave, frames, searcharea=None, samp=240, thresh=60, bgi
             img_strobe[keeppixels,:] = img_c1[keeppixels,:]
             
         #%% if not a strobe frame
-        if cnt < frames.iloc[0]:
+        if (cnt < frames.iloc[0]):
             # set current frame
             cap.set(1, cnt)
             ret, img = cap.read()
@@ -134,17 +134,54 @@ def strobe(filename, filesave, frames, searcharea=None, samp=240, thresh=60, bgi
             # set background (subtraction frame)
             cap.set(1,cnt+bgint)
             ret, img_bg = cap.read()
+            img_bg_c1 = img_bg.copy()
+            # if frame is not after the last strobe
+            if (searcharea is not None) and (cnt < frames.iloc[-1]):
+                # identify previous search area
+                crop1 = searcharea[frames.iloc[strobeframe]]
+                # identify next search area
+                crop2 = searcharea[frames.iloc[strobeframe+1]]
+                # create the widest search area from the two given search areas
+                left = np.min([crop1[0][0],crop1[1][0],crop2[0][0],crop2[1][0]])
+                right = np.max([crop1[0][0],crop1[1][0],crop2[0][0],crop2[1][0]])
+                top = np.min([crop1[0][1],crop1[1][1],crop2[0][1],crop2[1][1]])
+                bottom = np.max([crop1[0][1],crop1[1][1],crop2[0][1],crop2[1][1]])
+                crop = [(left,top),(right,bottom)]
+                # set base black image, then store image
+                img1 = np.zeros(img.shape,'uint8')
+                img1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_c1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+                # set base black image, then store image
+                img_bg = np.zeros(img.shape,'uint8')
+                img_bg[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_bg_c1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+            else: # if frame is after last strobe frame
+                # if object is moving left
+                if searcharea[frames.iloc[strobeframe]][0][0] - searcharea[frames.iloc[strobeframe-1]][0][0] < 0:
+                    # identify last search area
+                    crop1 = searcharea[frames.iloc[strobeframe]]
+                    # create the widest search area from the two given search areas
+                    crop = [(1,crop1[0][1]-50),(crop1[1][0],crop1[1][1]+50)]
+                else: # if object is moving right
+                    # identify last search area
+                    crop1 = searcharea[frames.iloc[strobeframe]]
+                    # create the widest search area from the two given search areas
+                    crop = [(crop1[0][0],crop1[0][1]-50),(frame_width,crop1[1][1]+50)]
+                # set base black image, then store image
+                img1 = np.zeros(img.shape,'uint8')
+                img1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_c1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
+                # set base black image, then store image
+                img_bg = np.zeros(img.shape,'uint8')
+                img_bg[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]] = img_bg_c1[crop[0][1]:crop[1][1],crop[0][0]:crop[1][0]]
             # find absolute difference between frames and then sum along BGR axis
-            diff = np.sum(cv2.absdiff(img_bg, img),axis=2)
+            diff = np.sum(cv2.absdiff(img_bg, img1),axis=2)
             # dilate image to fill holes
             keeppixels = ndimage.binary_dilation(diff>thresh, iterations=3)
             # set base black image, then store image
             img_cur = np.zeros(img.shape,'uint8')
             img_cur[keeppixels,:] = img_c1[keeppixels,:]
             # place current strobe image on top of video image
-            img[img_new>0] = img_new[img_new>0]
+            img[np.sum(img_new,axis=2)>0,:] = img_new[np.sum(img_new,axis=2)>0,:]
             # place current strobe image on top of video image
-            img[img_cur>0] = img_cur[img_cur>0]
+            img[np.sum(img_cur,axis=2)>0,:] = img_cur[np.sum(img_cur,axis=2)>0,:]
             if ret == True:
                 # write the frame into the file
                 out.write(img)
