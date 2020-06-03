f3521f2eedc9ec1ca57bd64ebca74a60bb0b4375
cwiens32 <cwiens32@gmail.com>
2020-06-03 11:19:50 -0500

    Make variables cumulative/continuous instead of discrete

--- Impulse_Velocity.py
+++ Impulse_Velocity.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Impulse_Velocity
-    Calculate impulse and change in velocity.
+    Calculate impulse and change in velocity throughout duration.
 
 Input
     data: DATAFRAME Mx3 dataframe of force data (includes X, Y, and Z !)
-    BW: FLOAT participant's body weight in Newtons
-    samp: INT sampling rate of force plate
+    bw: FLOAT participant's body weight in Newtons
+    samp: INT sampling rate of force plate (default: 1200)
     
 Output
     imp: DATAFRAME net impulse of X, Y, Z, and positive impulse Z (Ns)
@@ -22,18 +22,18 @@ Created on Fri Nov  8 20:06:55 2019
 
 import pandas as pd
 
-def imp_vel(data, BW, samp):
+def imp_vel(data, bw, samp=1200):
     
     # column names
     col = data.columns
     
     ### calculate impulse
-    imp = pd.DataFrame({col[0]: [data[col[0]].sum()/samp],
-                        col[1]: [data[col[1]].sum()/samp],
-                        col[2]: [(data[col[2]]-BW).sum()/samp],
-                        col[2] + '_positive': [data[col[2]].sum()/samp]})
+    imp = pd.DataFrame({col[0]: data[col[0]].cumsum()/samp,
+                        col[1]: data[col[1]].cumsum()/samp,
+                        col[2]: (data[col[2]]-bw).cumsum()/samp,
+                        col[2] + '_positive': data[col[2]].cumsum()/samp})
     
     ### calculate change in velocity
-    velD = imp[col[0:3]] / (BW / 9.81)
+    velD = imp[col[0:3]] / (bw / 9.81)
     
     return imp, velD
