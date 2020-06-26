cee43cd691abebe2a2206fc2751a02ad7d9a1796
cwiens32 <cwiens32@gmail.com>
2020-06-26 16:38:22 -0500

    Keep frame or time column the same as original

--- derivative.py
+++ derivative.py
@@ -32,17 +32,26 @@ def centraldiff(f, dt):
     #%% initialize derived data
     d = pd.DataFrame(np.zeros(f.shape, dtype=float), columns=f.columns, index=f.index)
     
+    #%% do not do calculations on frame or time column (if exists)
+    cols = [c for c in d.columns if c.lower() not in ["frame", "time"]]
+    fortcol = [c for c in d.columns if c.lower() in ["frame", "time"]]
+    
+    #%% find first and last valid index
+    ind_first = d.first_valid_index()
+    ind_last = d.last_valid_index()
+    
     #%% foward difference for first frame
-    d.iloc[0,:] = (f.iloc[1,:] - f.iloc[0,:]) / dt
+    d.loc[ind_first, cols] = (f.loc[ind_first+1, cols] - f.loc[ind_first, cols]) / dt
     
     #%% central difference for all but first and last frame
-    for cnt in range(1,len(f)-1):
-        d.iloc[cnt,:] = (f.iloc[cnt+1,:] - f.iloc[cnt-1,:]) / (2*dt)
+    for cnt in range(ind_first+1,ind_last):
+        d.loc[cnt, cols] = (f.loc[cnt+1, cols] - f.loc[cnt-1, cols]) / (2*dt)
     
     #%% backward difference for last frame
-    d.iloc[len(f)-1,:] = (f.iloc[-1,:] - f.iloc[-2,:]) / dt
+    d.loc[ind_last, cols] = (f.loc[ind_last, cols] - f.loc[ind_last-1, cols]) / dt
     
-    
+    #%% add back frame or time column
+    d[fortcol] = f[fortcol]
     
     #%% export data
     return d
