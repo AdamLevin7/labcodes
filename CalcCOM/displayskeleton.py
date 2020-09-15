c765e7be110adb19e3a0a574d795498f93619532
cwiens32 <cwiens32@gmail.com>
2020-09-15 09:51:40 -0700

    Update displayskeleton.py

--- CalcCOM/displayskeleton.py
+++ CalcCOM/displayskeleton.py
@@ -69,41 +69,49 @@ def addskeleton(file_vid, data, data_cm, segments,
         data_cm.loc[:, data_cm.columns.str.contains('_y')] = frame_height - data_cm.loc[:, data_cm.columns.str.contains('_y')]
     
     #%% create video
-    # frame number 
-    framenum = 0
+    # find all frame numbers
+    frame_num_list = data['frame']
+    # create frame counter
+    frame_cnt = 0
+    # create frame list counter
+    frame_list_cnt = 0
+    
     while(True):
         ret, frame = cap.read()
         if ret == True:
             
             #%% apply skeleton on each image
             # if current frame is in data
-            if framenum in range(int(data['frame'].iloc[0]),int(data['frame'].iloc[-1])):
+            if any(frame_num_list.isin([frame_cnt])):
+                
+                # find what index current frame is
+                frameloc = frame_num_list.isin([frame_cnt]).idxmax()
                 
                 #%% loop through digitized points
                 for cnts in range(len(segments)):
                     # if segment doesn't contain nans
-                    if (any(np.isnan(data.filter(regex = segments['origin'][cnts]).loc[framenum,:])) or
-                        any(np.isnan(data.filter(regex = segments['other'][cnts]).loc[framenum,:]))):
+                    if (any(np.isnan(data.filter(regex = segments['origin'][cnts]).loc[frameloc,:])) or
+                        any(np.isnan(data.filter(regex = segments['other'][cnts]).loc[frameloc,:]))):
                         pass
                     else:
                         # if it is head
                         if (segments.iloc[cnts,:]).name == 'head':
                             # origin
-                            orig_loc = tuple(data.filter(regex = segments['origin'][cnts]).loc[framenum,:].astype(int))
+                            orig_loc = tuple(data.filter(regex = segments['origin'][cnts]).loc[frameloc,:].astype(int))
                             # other
-                            oth_loc = tuple(data.filter(regex = segments['other'][cnts]).loc[framenum,:].astype(int))
+                            oth_loc = tuple(data.filter(regex = segments['other'][cnts]).loc[frameloc,:].astype(int))
                             # draw segment with red line
                             frame = cv2.line(frame, orig_loc, oth_loc, (0,0,255), thickness=3)
                             # segment center of mass
                             segcm = data_cm.filter(regex = (segments.iloc[cnts,:]).name)
                             # display segment center of mass location
-                            segcm_loc = tuple(segcm.loc[framenum,:].astype(int))
+                            segcm_loc = tuple(segcm.loc[frameloc,:].astype(int))
                             frame = cv2.circle(frame, segcm_loc, 3, (0,0,0), -1)
                             
                         # if it is trunk
                         elif (segments.iloc[cnts,:]).name == 'trunk':
                             # origin
-                            orig_loc = tuple(data.filter(regex = segments['origin'][cnts]).loc[framenum,:].astype(int))
+                            orig_loc = tuple(data.filter(regex = segments['origin'][cnts]).loc[frameloc,:].astype(int))
                             # other
                             oth = data.filter(regex = segments['other'][cnts])
                             # if both hips were located, use average
@@ -111,13 +119,13 @@ def addskeleton(file_vid, data, data_cm, segments,
                                 oth = pd.DataFrame({'x': oth.filter(regex='x').mean(axis = 1),
                                                     'y': oth.filter(regex='y').mean(axis = 1)})
                             # create tuple for other
-                            oth_loc = tuple(oth.loc[framenum,:].astype(int))
+                            oth_loc = tuple(oth.loc[frameloc,:].astype(int))
                             # draw segment with red line
                             frame = cv2.line(frame, orig_loc, oth_loc, (0,0,255), thickness=3)
                             # segment center of mass
                             segcm = data_cm.filter(regex = (segments.iloc[cnts,:]).name)
                             # display segment center of mass location
-                            segcm_loc = tuple(segcm.loc[framenum,:].astype(int))
+                            segcm_loc = tuple(segcm.loc[frameloc,:].astype(int))
                             frame = cv2.circle(frame, segcm_loc, 3, (0,0,0), -1)
                     
                         # if another segment
@@ -132,9 +140,9 @@ def addskeleton(file_vid, data, data_cm, segments,
                                 orig_l = orig.filter(regex = 'left')
                                 orig_r = orig.filter(regex = 'right')
                                 # find left and right segments
-                                segcm_l = tuple(segcm.filter(regex = 'left').loc[framenum,:].astype(int))
-                                segcm_r = tuple(segcm.filter(regex = 'right').loc[framenum,:].astype(int))
-                            # other
+                                segcm_l = tuple(segcm.filter(regex = 'left').loc[frameloc,:].astype(int))
+                                segcm_r = tuple(segcm.filter(regex = 'right').loc[frameloc,:].astype(int))
+                            # frameloc
                             oth = data.filter(regex = segments['other'][cnts])
                             # find if location exists in digitized data set
                             if len(oth.columns) > 0:
@@ -147,8 +155,8 @@ def addskeleton(file_vid, data, data_cm, segments,
                                 # if left segment exists
                                 if (len(orig_l.columns)>0 or len(oth_l.columns)>0):
                                     # create tuple
-                                    orig_loc = tuple(orig_l.loc[framenum,:].astype(int))
-                                    oth_loc = tuple(oth_l.loc[framenum,:].astype(int))
+                                    orig_loc = tuple(orig_l.loc[frameloc,:].astype(int))
+                                    oth_loc = tuple(oth_l.loc[frameloc,:].astype(int))
                                     # draw segment with red line
                                     frame = cv2.line(frame, orig_loc, oth_loc, (0,0,255), thickness=3)
                                     # display segment center of mass location
@@ -156,8 +164,8 @@ def addskeleton(file_vid, data, data_cm, segments,
                                 # if right segment exists
                                 if (len(orig_r.columns)>0 or len(oth_r.columns)>0):
                                     # create tuple
-                                    orig_loc = tuple(orig_r.loc[framenum,:].astype(int))
-                                    oth_loc = tuple(oth_r.loc[framenum,:].astype(int))
+                                    orig_loc = tuple(orig_r.loc[frameloc,:].astype(int))
+                                    oth_loc = tuple(oth_r.loc[frameloc,:].astype(int))
                                     # draw segment with red line
                                     frame = cv2.line(frame, orig_loc, oth_loc, (0,0,255), thickness=3)
                                     # display segment center of mass location
@@ -165,33 +173,36 @@ def addskeleton(file_vid, data, data_cm, segments,
                                 # if neither left or right exists
                                 if (len(orig_l.columns)==0) and (len(orig_r.columns)==0):
                                     # create tuple
-                                    orig_loc = tuple(orig.loc[framenum,:].astype(int))
-                                    oth_loc = tuple(orig.loc[framenum,:].astype(int))
+                                    orig_loc = tuple(orig.loc[frameloc,:].astype(int))
+                                    oth_loc = tuple(orig.loc[frameloc,:].astype(int))
                                     # draw segment with red line
                                     frame = cv2.line(frame, orig_loc, oth_loc, (0,0,255), thickness=3)
                                     # display segment center of mass location
-                                    segcm_loc = tuple(segcm.loc[framenum,:].astype(int))
+                                    segcm_loc = tuple(segcm.loc[frameloc,:].astype(int))
                                     frame = cv2.circle(frame, segcm_loc, 3, (0,0,0), -1)
                         
                         
                 #%% display center of mass
                 # if center of mass is not nan (from missing segment)
-                if not any(np.isnan(data_cm[['body_x','body_y']].loc[framenum])):
+                if not any(np.isnan(data_cm[['body_x','body_y']].loc[frameloc])):
                     # create tuple
-                    bodycm_loc = tuple(data_cm.filter(regex = 'body').loc[framenum,:].astype(int))
+                    bodycm_loc = tuple(data_cm.filter(regex = 'body').loc[frameloc,:].astype(int))
                     # display center of mass location
                     frame = cv2.circle(frame, bodycm_loc, 8, (0,255,255), -1)
+                
+                """ iterate counter """
+                frame_list_cnt += 1
             
             
             #%% save frame and add to video
             # create frame name
             framename = os.path.join(savefolder,
-                                     os.path.basename(file_vid)[ :-4] + '_' + str(framenum) + '.png')
+                                     os.path.basename(file_vid)[ :-4] + '_' + str(frame_cnt) + '.png')
             cv2.imwrite(framename, frame)
             # write the frame into the file
             vid_out.write(frame)
             # iterate frame number
-            framenum += 1
+            frame_cnt += 1
         else:
             break
             
