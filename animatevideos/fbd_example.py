a104d2693f06071d45a8498ea632f74fdffbb769
cwiens32 <cwiens32@gmail.com>
2020-10-23 10:28:57 -0700

    Add function to convert to and display flexion/extension moments

--- animatevideos/fbd_example.py
+++ animatevideos/fbd_example.py
@@ -7,6 +7,7 @@ Created on Mon Sep 14 10:55:44 2020
 
 from animatevideos.fbd_display import fbd_vis
 import pandas as pd
+from NJM.jointkinetics import convert_to_flexext_ref
 
 """ all the data came from a previously processed example
     currently, it is assuming you are in the animatevideos folder. if you are
@@ -20,6 +21,17 @@ data_cm = pd.read_excel(data_file, sheet_name='data_cm', index_col=0)
 data_njm = pd.read_excel(data_file, sheet_name='data_njm', index_col=0)
 
 
+""" set anterior
+    This is the direction the individual is facing
+    It is used to set positive/negative values to extensor/flexor moments
+"""
+anterior = 'left'
+
+""" convert NJM data to flexor/extensor references 
+    This makes extensor moments positive and flexor moments negative
+"""
+data_njm_update = convert_to_flexext_ref(data_njm, anterior=anterior)
+
 """ set side:
     This is the side of the body in which the joint kinetics were calculated
 """
@@ -30,10 +42,17 @@ side = 'left'
 """
 cnt = 4326
 
+""" set colorlegend (what does the color scheme represent):
+    This is what will be displayed in the legend, describing what a positive/negative moment means
+    'flexext' would say Extensor = positive and Flexor = negative
+    'posneg' would say Positive = positive (CCW) and Negative = negative (CW)
+"""
+colorlegend = 'flexext'
+
 """
 create object
 """
-fbd_obj = fbd_vis(data_force, data_digi, data_cm, data_njm, side, cnt)
+fbd_obj = fbd_vis(data_force, data_digi, data_cm, data_njm_update, side, cnt, colorlegend=colorlegend)
 
 """
 if creating visual at one instant...
