8a4035f5392d0c3bb0e2d671df2ddb79a8b66544
cwiens32 <cwiens32@gmail.com>
2020-12-09 16:51:34 -0600

    Add pix2m_from2cones

--- pixelratios.py
+++ pixelratios.py
@@ -4,20 +4,47 @@ pixelratios
     Calculate ratios involving pixels
     
 Modules:
+    pix2m_from2cones: Calculate ratio pixels to meters using two cones (or other object).
     pix2m_fromplate: Calculate ratio pixels to meters using plate dimensions.
     forcemagpixel: Calculate ratio of body weight to pixels.
     
 Dependencies:
     numpy
+    findpoint
 
 Created on Mon Apr 13 14:38:03 2020
 
 @author: cwiens, Casey Wiens, cwiens32@gmail.com
 """
 
-import numpy as np
 
-#%%
+"""
+pix2m_from2cones
+    Calculate ratio pixels to meters using two cones (or other object).
+
+Input:
+    video_cal: STRING full path of video file to use for selecting cone/object location
+    cal_dist: FLOAT distance in meters that the cones/objects are apart [m]
+
+Output:
+    pix2m: DICT ratio of pixels:meter
+
+Dependencies:
+    findpoint
+"""
+def pix2m_from2cones(video_cal, cal_dist):
+    from findpoint import clickpoint
+
+    """ identify the two cone locations from calibration video """
+    x, y = clickpoint(video_cal, label="Select left then right cone and press escape")
+
+    """ calculate pixel 2 meter ratio """
+    pix2m = cal_dist / (x[1] - x[0])
+
+    return pix2m
+
+
+
 """
 pix2m_fromplate
     Calculate ratio pixels to meters using plate dimensions.
@@ -33,6 +60,8 @@ Dependencies:
     numpy
 """
 def pix2m_fromplate(plate_area, plate_dim):
+    import numpy as np
+
     pix2m = {'x': plate_dim[0] / np.mean([plate_area[0][3][0]-plate_area[0][0][0],
                                           plate_area[0][2][0]-plate_area[0][1][0]]),
              
