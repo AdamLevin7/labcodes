18189921f214f81bedac3efa228837d0b203952c
hestewar <hestewar@usc.edu>
2021-08-17 20:26:51 -0700

    Updates to document function, completed example
    Next step should make it a real function

--- document_fxn.R
+++ document_fxn.R
@@ -12,13 +12,16 @@
 # Gather input variables or make a normal function*
 # Ask for input if nothing is provided
 
-script_name = "blah"
-function_name = "blah_function"
-script_website = 'blah_blah.com'
+script_name <- "blah"
+function_name <- "blah_function"
+script_website <- 'blah_blah.com'
 keywords <- "blah_keyword"
+describe_fxn <- "describe blah blah blah"
+depend_list <- "dEPenDenCies"
+inputs <- "input1 , input 2"
+outputs <- "output 1, output 7"
 
 # Concatenate the lines together
-
 
 cat(paste0("
 ## Script: ", script_name, "\n",
@@ -26,38 +29,35 @@ cat(paste0("
 "[Link to ", script_name, " Code](", script_website, ")", "\n",
 "\n",
 "### **Keywords:**", "\n",
-keywords))
+keywords, "\n",
+"\n",
+"### **Syntax:**", "\n",
+"```" , "\n",
+"from ", script_name," import ", function_name, "\n",
+"\n",
+outputs, " = ", function_name, "(", inputs, ")", "\n",
+"```" , "\n",
+"### Dependencies", "\n",
+ depend_list, "\n",
+   # TODO make a for loop here that allows you to format multiple dependencies
+"\n",
+"### **Description:**", "\n",
+describe_fxn, "\n",
+"\n",
+"### **Arguments:**","\n",
+"\n",
+"#### *Inputs*", "\n",
+   # TODO make a for loop to format the inputs the way you want them
+   inputs, "\n",
+"\n",
+"#### *Outputs*", "\n",
+   # TODO make a for loop to format the outputs the way you want them
+   outputs, "\n",
+"\n",
+"### **Examples:**", "\n",
+"Helpful examples", "\n",
+"\n",
+"[Back to Table of Contents](#table-of-contents)"
+))
 
-### **Syntax:**
-```
-from ", script_name," import", function_name,"
-
-", outputs, " = ", function_name, "(", inputs, ")",
-```
-### Dependencies
-* **scipy:**
-* **np**: numpy
-* **centraldiff:** USC BRL Function from derivative script
-
-### **Description:**<br/>
-Filter all columns of a dataframe using a spine interpolation method and filtfilt (zero-phase)
-
-### **Arguments:**
-
-#### *Inputs*
-
-* **df:** DATAFRAME data with columns to be filtered (first column will not be)
-* **b:** ARRAY_LIKE numerator coefficient vector of the filter
-* **a:** ARRAY_LIKE denominator coefficient vector of the filter
-
-#### *Outputs*
-
-   * **filt_data:** DF Filtered input dataframe
-
-### **Examples:**
-Helpful examples
-
-[Back to Table of Contents](#table-of-contents)
-
-"
 
