ffbdc85b41b1f55272b6f90e3ac35a2481ab33c7
cwiens32 <cwiens32@gmail.com>
2021-10-20 10:45:58 -0700

    Add bounding box method

--- capture_area.py
+++ capture_area.py
@@ -26,14 +26,16 @@ Created on Wed Oct 30 10:06:19 2019
 @author: cwiens, Casey Wiens, cwiens32@gmail.com
 """
 
-# import the necessary packages 
-import cv2
+# import the necessary packages
 
 
-def findarea(video,frame=0,label='frame'):
+def findarea(video, frame=0, label='frame', method="boundingbox"):
     
     import wx
     import sys
+    import cv2
+    import matplotlib.pyplot as plt
+    from matplotlib.widgets import RectangleSelector, Button
     
     # idenify screen resolution
     app = wx.App(False)
@@ -42,6 +44,16 @@ def findarea(video,frame=0,label='frame'):
     
     # set label to show
     labelshow = 'a: advance, w: recrop. ' + label
+
+    # load in video and set to specific frame
+    cap = cv2.VideoCapture(video)
+    cap.set(1,frame)
+    ret, frame = cap.read()
+
+    # resize window
+    (h, w) = frame.shape[:2]
+    r = height * 0.75 / float(h)
+    dim = (int(w * r), int(height * 0.75))
     
     global ref_point
     
@@ -66,36 +78,80 @@ def findarea(video,frame=0,label='frame'):
     		# draw a rectangle around the region of interest 
     		cv2.rectangle(frame, ref_point[0], ref_point[1], (0, 255, 0), 2)
     		cv2.imshow(labelshow, frame)
-             
-    
-    # load the image, clone it, and setup the mouse callback function    
-    cap = cv2.VideoCapture(video)
-    cap.set(1,frame)
-    ret, frame = cap.read()
-    clone = frame.copy()
-    # resize window
-    (h, w) = frame.shape[:2]
-    r = height*0.75 / float(h)
-    dim = (int(w*r), int(height*0.75))
-    cv2.namedWindow(labelshow, cv2.WINDOW_NORMAL)
-    cv2.setMouseCallback(labelshow, shape_selection)
-    cv2.resizeWindow(labelshow, dim)
-    
-    # keep looping until the 'q' key is pressed 
-    while True:
-        cv2.imshow(labelshow, frame)
-        key = cv2.waitKey(1) & 0xFF
+
+    def line_select_callback(eclick, erelease):
+        # grab references to the global variables
+        global ref_point
+
+        ref_point = [(int(eclick.xdata), int(eclick.ydata))] # x1, y1
+        ref_point.append((int(erelease.xdata), int(erelease.ydata))) # x2, y2
+
+    def validate_crop(*args):
+        fig.canvas.stop_event_loop()
+
+    def display_help(*args):
+        print(
+            "1. Use left click to select the region of interest. A green box will be drawn around the selected region.\n\n"
+            "2. Use the corner points to expand the box and center to move the box around the image.\n\n"
+            "3. Click"
+        )
+
+    """ select area methods """
+    if method == "boundingbox":
+        " bounding box version - modified from DeepLabCut auxfun_videos.py from DLC 2.2b7 "
+        px = 1 / plt.rcParams['figure.dpi']  # pixel in inches
+        fig = plt.figure(figsize=(dim[0]*px, dim[1]*px))
+        ax = fig.add_subplot(111)
+        ax.imshow(frame[:, :, ::-1])
+        ax.axis('off')
+        plt.title('Click and drag to select area. Modify if needed. Then click the "Crop" button')
+        ax_help = fig.add_axes([0.9, 0.2, 0.1, 0.1])
+        ax_save = fig.add_axes([0.9, 0.1, 0.1, 0.1])
+        crop_button = Button(ax_save, "Crop")
+        crop_button.on_clicked(validate_crop)
+        help_button = Button(ax_help, "Help")
+        help_button.on_clicked(display_help)
+
+        # create rectangular selection
+        rs = RectangleSelector(
+            ax,
+            line_select_callback,
+            drawtype="box",
+            minspanx=5,
+            minspany=5,
+            interactive=True,
+            spancoords="pixels",
+            rectprops=dict(facecolor="green", edgecolor="black", alpha=0.3, fill=True),
+        )
+        plt.show()
+
+        fig.canvas.start_event_loop(timeout=-1)
+
+        plt.close(fig)
+
+    elif method == "clickdrag":
+        " old version - click and drag with redo option"
+        # load the image, clone it, and setup the mouse callback function
+        clone = frame.copy()
+        cv2.namedWindow(labelshow, cv2.WINDOW_NORMAL)
+        cv2.setMouseCallback(labelshow, shape_selection)
+        cv2.resizeWindow(labelshow, dim)
+
+        # keep looping until the 'q' key is pressed
+        while True:
+            cv2.imshow(labelshow, frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            # press 'w' to reset the window
+            if key == ord("w"):
+                frame = clone.copy()
+
+            # if the 'a' key is pressed, break from the loop
+            elif key == ord("a"):
+                break
         
-        # press 'w' to reset the window
-        if key == ord("w"):
-            frame = clone.copy()
-            
-        # if the 'a' key is pressed, break from the loop
-        elif key == ord("a"):
-            break
-        
-    # close all open windows
-    cv2.destroyAllWindows()
+        # close all open windows
+        cv2.destroyAllWindows()
 
     return ref_point
 
@@ -106,8 +162,9 @@ draw_bbox
 
 Copied from DeepLabCut auxfun_videos.py from DLC 2.2b7
 """
-def draw_bbox(video):
-    
+def draw_bbox(video, frame):
+
+    import cv2
     import matplotlib.pyplot as plt
     from matplotlib.widgets import RectangleSelector, Button
 
@@ -119,25 +176,33 @@ def draw_bbox(video):
     success = False
     # Read the video until a frame is successfully read
     while not success:
+        #%% display current frame
+        clip.set(1,frame)
         success, frame = clip.read()
 
     bbox = [0, 0, frame.shape[1], frame.shape[0]]
 
     def line_select_callback(eclick, erelease):
-        bbox[:2] = int(eclick.xdata), int(eclick.ydata)  # x1, y1
-        bbox[2:] = int(erelease.xdata), int(erelease.ydata)  # x2, y2
+        #bbox[:2] = int(eclick.xdata), int(eclick.ydata)  # x1, y1
+        #bbox[2:] = int(erelease.xdata), int(erelease.ydata)  # x2, y2
+        bbox = [(int(eclick.xdata), int(eclick.ydata))] # x1, y1
+        bbox.append((int(erelease.xdata), int(erelease.ydata))) # x2, y2
 
     def validate_crop(*args):
         fig.canvas.stop_event_loop()
 
     def display_help(*args):
         print(
-            "1. Use left click to select the region of interest. A red box will be drawn around the selected region. \n\n2. Use the corner points to expand the box and center to move the box around the image. \n\n3. Click "
+            "1. Use left click to select the region of interest. A green box will be drawn around the selected region.\n\n"
+            "2. Use the corner points to expand the box and center to move the box around the image.\n\n"
+            "3. Click"
         )
 
+    #
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.imshow(frame[:, :, ::-1])
+    ax.axis('off')
     ax_help = fig.add_axes([0.9, 0.2, 0.1, 0.1])
     ax_save = fig.add_axes([0.9, 0.1, 0.1, 0.1])
     crop_button = Button(ax_save, "Crop")
@@ -145,6 +210,7 @@ def draw_bbox(video):
     help_button = Button(ax_help, "Help")
     help_button.on_clicked(display_help)
 
+    # create rectangular selection
     rs = RectangleSelector(
         ax,
         line_select_callback,
@@ -153,16 +219,11 @@ def draw_bbox(video):
         minspany=5,
         interactive=True,
         spancoords="pixels",
-        rectprops=dict(facecolor="red", edgecolor="black", alpha=0.3, fill=True),
+        rectprops=dict(facecolor="green", edgecolor="black", alpha=0.3, fill=True),
     )
     plt.show()
 
-    # import platform
-    # if platform.system() == "Darwin":  # for OSX use WXAgg
-    #    fig.canvas.start_event_loop(timeout=-1)
-    # else:
-    fig.canvas.start_event_loop(timeout=-1)  # just tested on Ubuntu I also need this.
-    #    #fig.canvas.stop_event_loop()
+    fig.canvas.start_event_loop(timeout=-1)
 
     plt.close(fig)
     return bbox
