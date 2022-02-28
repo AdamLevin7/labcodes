1e1baef39c7136be3cc5c9e1552ccd827b93c384
hestewar <hestewar@usc.edu>
2022-02-28 12:09:10 -0800

    Documentation, moment inertia

--- kinematics/calc_segmentmomentinertia.py
+++ kinematics/calc_segmentmomentinertia.py
@@ -1,30 +1,39 @@
-# -*- coding: utf-8 -*-
 """
-calc_segmentmomentinertia
-    Calculate moment of inertia for each segment.
-    
-Inputs
-    data: DATAFRAME digitized data for each segment
-        Column 0: time or frame number
-        Column 1+: length of the segment
-    segments: DATAFRAME segment parameters obtained from segdim_deleva.py
-    
-Outputs
-    dataout: DATAFRAME moment of inertia of each segment
-    
-Dependencies
-    pandas
-    numpy
-    
-Created on Sat Apr 25 13:06:44 2020
+Script: calc_segmentmomentinertia
+    Calculate the moment of inertia for a segment.
 
-@author: cwiens
+Modules
+    momentinertia: Calculate moment of inertia for each segment.
+
+Author:
+    Casey Wiens
+    cwiens32@gmail.com
 """
 
-import pandas as pd
-import numpy as np
 
 def momentinertia(datain, segments, mass):
+    """
+    Function::: momentinertia
+    	Description: brief description here (1 line)
+    	Details: Full description with details here
+
+    Inputs
+        data: DATAFRAME digitized data for each segment
+            Column 0: time or frame number
+            Column 1+: length of the segment
+        segments: DATAFRAME segment parameters obtained from segdim_deleva.py
+
+    Outputs
+        dataout: DATAFRAME moment of inertia of each segment
+
+    Dependencies
+        pandas
+        numpy
+    """
+
+    # Dependencies
+    import pandas as pd
+    import numpy as np
     
     # initialize data out
     dataout = pd.DataFrame(datain.iloc[:,0])
@@ -38,7 +47,5 @@ def momentinertia(datain, segments, mass):
         i = (segments['massper'].iloc[cnt]*mass) * np.square((seg * segments['r_gyr'].iloc[cnt]))
         # join with new dataframe
         dataout = dataout.join(i)
-            
-    
-    
+
     return dataout
